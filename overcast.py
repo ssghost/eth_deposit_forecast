33ad1755b952d42440a83835565ae7d9d2868c62
ssghost <ssprof0@gmail.com>
2024-01-04 12:14:35 +0800

    add overcast

--- overcast.py
+++ overcast.py
@@ -1,18 +1,7 @@
 import vectorbt as vbt
-vbt.settings.set_theme("dark")
-SYMBOL = "BTC-USD"
-TIMEFRAME = "1 hour"
-START = "one year ago"
 
-LAST_N_BARS = 66
-PRED_N_BARS = 12
 
-GIF_FNAME = "projections.gif"
-GIF_N_BARS = 72
-GIF_FPS = 4
-GIF_PAD = 0.01
-
-data = vbt.YFData.pull(SYMBOL, timeframe=TIMEFRAME, start=START)
+data = vbt.YFData.pull("ETH-USD")
 def find_patterns(data):
     price = data.hlc3
     pattern = price.values[-LAST_N_BARS:]
