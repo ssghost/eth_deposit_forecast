291ef9885041d338c6d30dc8a514bb3576cc66f9
ssghost <ssprof0@gmail.com>
2024-01-04 16:53:23 +0800

    mod overcast

--- overcast.py
+++ overcast.py
@@ -2,16 +2,16 @@ import vectorbt as vbt
 import pandas as pd
 import json
 
-def find_patterns(data):
+def main() -> None:
+    data = vbt.YFData.pull("ETH-USD")
     pattern_ranges = data.hlc3.vbt.find_pattern(
         pattern=data.close.iloc[-10:],
         rescale_mode="rebase",
         overlap_mode="allow"
     ).status_closed
-    return pattern_ranges
-
-if __name__=='__main__':
-    data = vbt.YFData.pull("ETH-USD")
-    pattern_ranges = find_patterns(data)
     with open("castdata.json", 'w') as f:
         json.dump(pattern_ranges.to_json(), f)
+
+if __name__ == '__main__':
+    main()
+    
