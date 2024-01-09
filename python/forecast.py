96b87b180079832c1a2399d5daf1eb87839dbe4a
ssghost <ssprof0@gmail.com>
2024-01-09 16:18:32 +0800

    add save model

--- python/forecast.py
+++ python/forecast.py
@@ -1,8 +1,9 @@
 from train import *
+from torch import load
 import json
 
 def forecast() -> None:
-    model = train()
+    model = load("../model.pt")
     e_data = vbt.YFData.download("ETH-USD", start="5 days ago UTC").get('Close')
     predictions = model.predict(e_data.test_dataloader())
     with open("../castdata.json", 'w') as f:
